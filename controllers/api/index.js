df92dddf0a2866185d5eaed1933e0c35aeb62114
Matt Burke <burkemm84@gmail.com>
2021-11-05 14:21:22 -0500

    Delete controllers directory

--- controllers/api/index.js
+++
@@ -1,9 +0,0 @@
-const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const postRoutes = require('./postRoutes');
-// These are the routes I'll be using.
-router.use('/users', userRoutes);
-router.use('/posts', postRoutes);
-
-
-module.exports = router;
